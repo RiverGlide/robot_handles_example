09c76cdc9b08ea5ad64316b77f8de5cac9210842
Andy Palmer <andy@andypalmer.com>
2016-06-18 17:50:07 +0100

    The annotation scanning is now only done once per JVM run, but the tests still run slow (probably the overhead of starting the browser)

--- src/test/java/com/riverglide/demo/robot_handles/Robot.java
+++ src/test/java/com/riverglide/demo/robot_handles/Robot.java
@@ -1,36 +1,21 @@
 package com.riverglide.demo.robot_handles;
 
 import com.riverglide.demo.robot_handles.handles.IAnswerQuestions;
-import com.riverglide.demo.robot_handles.handles.RobotHandleFor;
-import com.riverglide.demo.robot_handles.problems.NotARobotHandleComplaint;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.reflections.Reflections;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import static java.lang.String.format;
 
 public class Robot {
-    private final Map<String, IAnswerQuestions> available_handles;
-
-    public Robot() {
-        available_handles = new HashMap<>();
-        Set<Class<?>> known_handles = new Reflections().getTypesAnnotatedWith(RobotHandleFor.class);
-        known_handles.forEach(
-            handle -> available_handles.put(handle.getAnnotation(RobotHandleFor.class).value(), create(handle))
-        );
-    }
-
     public void exchange(Map<String, String> questions_and_answers, WebDriver with_user) {
         List<WebElement> questions_from_user = with_user.findElements(By.cssSelector("[data-question]"));
         for(WebElement question : questions_from_user) {
             String question_type = question.getAttribute("data-question-type");
-            IAnswerQuestions question_handler = available_handles.get(question_type);
+            IAnswerQuestions question_handler = RobotHandle.for_(question_type);
             if(question_handler == null) {
                 System.out.println(format("I don't know how to answer a question of type [%s]", question_type));
                 continue;
@@ -38,18 +23,6 @@ public class Robot {
 
             String answer = questions_and_answers.get(question.getAttribute("data-question"));
             question_handler.answer(question, answer);
-        }
-    }
-
-    private IAnswerQuestions create(Class<?> handle) {
-        try {
-            return (IAnswerQuestions) handle.newInstance();
-        }
-        catch (ClassCastException e) {
-            throw new NotARobotHandleComplaint(handle, e);
-        }
-        catch (Exception e) {
-            throw new UnsupportedOperationException("TODO - Handle this", e);
         }
     }
 }
---
+++ src/test/java/com/riverglide/demo/robot_handles/RobotHandle.java
@@ -0,0 +1,47 @@
+package com.riverglide.demo.robot_handles;
+
+import com.riverglide.demo.robot_handles.handles.IAnswerQuestions;
+import com.riverglide.demo.robot_handles.handles.RobotHandleFor;
+import com.riverglide.demo.robot_handles.problems.NotARobotHandleComplaint;
+import org.reflections.Reflections;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static java.lang.String.format;
+
+public class RobotHandle {
+    private static final IAnswerQuestions DONT_KNOW_HOW_TO_ANSWER =
+            (question, answer) -> System.out.println(format("I don't know how to answer a question in the form [%s]", question.getAttribute("data-question-type")));
+    private static Map<String, IAnswerQuestions> available_handles = new HashMap<>();
+
+    public static IAnswerQuestions for_(String question_type) {
+        if (available_handles.isEmpty()) { available_handles = findKnownRobotHandles(); }
+
+        return available_handles.getOrDefault(question_type, DONT_KNOW_HOW_TO_ANSWER);
+
+    }
+
+    private static Map<String, IAnswerQuestions> findKnownRobotHandles() {
+        System.out.println("Searching for robot handles");
+        Map<String, IAnswerQuestions> available_handles = new HashMap<>();
+        Set<Class<?>> known_handles = new Reflections().getTypesAnnotatedWith(RobotHandleFor.class);
+        known_handles.forEach(
+                handle -> available_handles.put(handle.getAnnotation(RobotHandleFor.class).value(), create(handle))
+        );
+        return available_handles;
+    }
+
+    private static IAnswerQuestions create(Class<?> handle) {
+        try {
+            return (IAnswerQuestions) handle.newInstance();
+        }
+        catch (ClassCastException e) {
+            throw new NotARobotHandleComplaint(handle, e);
+        }
+        catch (Exception e) {
+            throw new UnsupportedOperationException("TODO - Handle this", e);
+        }
+    }
+}
