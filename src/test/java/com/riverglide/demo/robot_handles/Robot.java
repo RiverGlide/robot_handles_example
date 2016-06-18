492fc80b8ee03b08020071954be04e3d61c275bd
Andy Palmer <andy@andypalmer.com>
2016-06-18 15:02:39 +0100

    Refactor to find appropriate question_handler by lookup

--- src/test/java/com/riverglide/demo/robot_handles/Robot.java
+++ src/test/java/com/riverglide/demo/robot_handles/Robot.java
@@ -5,22 +5,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static java.lang.String.format;
 
 public class Robot {
+    private static Map<String, HtmlSelect> available_handles = new HashMap<String, HtmlSelect>() {{
+        put("html-select", new HtmlSelect());
+    }};
+
     public void exchange(Map<String, String> questions_and_answers, WebDriver with_user) {
         List<WebElement> questions_from_user = with_user.findElements(By.cssSelector("[data-question]"));
         for(WebElement question : questions_from_user) {
             String question_type = question.getAttribute("data-question-type");
-            if(!question_type.equals("html-select")) {
+            HtmlSelect question_handler = available_handles.get(question_type);
+            if(question_handler == null) {
                 System.out.println(format("I don't know how to answer a question of type [%s]", question_type));
                 continue;
             }
-            HtmlSelect answerable = new HtmlSelect();
-            answerable.answer(question, "Groom");
+            question_handler.answer(question, "Groom");
         }
     }
 }
